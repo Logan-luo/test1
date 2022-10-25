5c199a9106915596c5887816345f9302cd09f4da
Logan-luo <50293344+Logan-luo@users.noreply.github.com>
2022-10-25 12:36:28 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,49 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/core.hpp>
 #include <opencv2/calib3d.hpp>
-
+#include <cmath>
 #include <iostream>
-
+#define g 9.8
+#define PAI 3.14
 using namespace std;
 using namespace cv;
+//求出两条直线交点
+Point2f setzero(Point2f a)
+{
+    a.x = 0;
+    a.y = 0;
+    return a;
+}
+float calangle(float distance, float high, float speed)
+{
+    float a = high / distance;
+    float x = distance, y = high;
+    float angle1 = atanf(a), angle2 = atanf(a);
+    float dy;
+    do
+    {
+        float a = cos(angle1), b = sin(angle1);
+        float time = x / 100 / speed * a;
+        dy = y - speed / 100 * b + 1 / 2 * g * pow(time, 2);
+        y = y + dy;
+        angle1 = atan2f(y, x);
+    } while (dy < 10);
+    return (angle1 - angle2) / (2 * PAI) * 360;
+}
+Point2f findcen(Point2f a, Point2f b, Point2f c, Point2f d)
+{
+    int x, y;
+    int X1 = a.x - b.x, Y1 = a.y - b.y, X2 = c.x - d.x, Y2 = c.y - d.y;
+    if (X1 * Y2 == X2 * Y1)
+        return Point((b.x + c.x) / 2, (b.y + c.y) / 2);
+    else
+    {
+        int A = X1 * a.y - Y1 * a.x, B = X2 * c.y - Y2 * c.x;
+        y = (A * Y2 - B * Y1) / (X1 * Y2 - X2 * Y1);
+        x = (B * X1 - A * X2) / (Y1 * X2 - Y2 * X1);
+        return Point2f(x, y);
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -14,6 +52,11 @@ int main(int argc, char *argv[])
         return -1;
     string path = argv[1];
     VideoCapture cap(path);
+    float fps = cap.get(CAP_PROP_FPS); // calculate the fps to get the time between each capture
+    float ftime = 1 / fps;
+    Point2f cent1, cent2;
+    setzero(cent1);
+    setzero(cent2);
     if (!cap.isOpened())
     {
         cout << "failed to open the video!" << endl;
@@ -30,7 +73,7 @@ int main(int argc, char *argv[])
         threshold(minus_channel, binary, 80, 255, THRESH_BINARY);
         // namedWindow("test1");
         //  imshow("Image", img);
-        imshow("test1", binary);
+        // imshow("test1", binary);
         findContours(binary, contours, RETR_EXTERNAL, RETR_LIST);
 
         vector<RotatedRect> tubes;
@@ -118,10 +161,10 @@ int main(int argc, char *argv[])
         vector<Point3f> objP;
         Mat objM;
         objP.clear();
-        objP.push_back(Point3f(15, 0, 0));
-        objP.push_back(Point3f(-15, 0, 0));
-        objP.push_back(Point3f(15, 130, 0));
-        objP.push_back(Point3f(-15, 130, 0));
+        objP.push_back(Point3f(15, -65, 0));
+        objP.push_back(Point3f(-15, -65, 0));
+        objP.push_back(Point3f(15, 65, 0));
+        objP.push_back(Point3f(-15, 65, 0));
 
         //初始化相机参数Opencv
         double camD[9] = {
@@ -142,12 +185,27 @@ int main(int argc, char *argv[])
         Mat rvec, tvec;
         solvePnP(objP, Points2D, camera_matrix, distortion_coefficients, rvec, tvec, false);
 
-        float a = sqrt((pow(tvec.at<double>(0, 0), 2) + pow(tvec.at<double>(0, 1), 2) + pow(tvec.at<double>(0, 2), 2)));
+        /*float dis = sqrt((pow(tvec.at<double>(0, 0), 2) + pow(tvec.at<double>(0, 1), 2) + pow(tvec.at<double>(0, 2), 2)));
 
-        cout << "The distance is " << a << endl;
+        cout << "The distance is " << dis << endl;*/
+
+        // using the distance to caculate the angle//
+        float xydis = sqrt((pow(tvec.at<double>(0, 0), 2) + pow(tvec.at<double>(0, 1), 2)));
+        float zdis = sqrt(pow(tvec.at<double>(0, 2), 2));
+        float angle = calangle(xydis, zdis, 30);
+        cout << "The angle is " << angle << endl;
+        // caculate the speed//
+        cent2 = findcen(fimid[0], semid[1], fimid[1], semid[0]);
+        float vx = (cent2.x - cent1.x) / 100 / ftime;
+        float vy = (cent2.y - cent1.y) / 100 / ftime;
+        cent1 = cent2;
+        Point2f cent3;
+        cent3.x = cent2.x + fps * vx;
+        cent3.y = cent2.y + fps * vy;
+        line(img, cent2, cent3, Scalar(0, 0, 255), 2);
+        putText(img, "The nest second of x and y will be" + to_string(cent3.x) + " " + to_string(cent3.y), Point(1, 20), FONT_HERSHEY_DUPLEX, 0.75, Scalar(255, 255, 255), 1);
         namedWindow("test2");
         imshow("test2", img);
-
         if (waitKey(30) == 27) // Esc
             if (waitKey(0) == 27)
                 break;
